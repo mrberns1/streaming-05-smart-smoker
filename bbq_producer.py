6784b06b6613553a51314b320f2a815a56444214
Missy Bernskoetter <s555988@nwmissouri.edu>
2023-02-15 15:38:03 -0600

    Adding codes

--- bbq_producer.py
+++ bbq_producer.py
@@ -27,14 +27,14 @@ def offer_rabbitmq_admin_site():
         webbrowser.open_new("http://localhost:15672/#/queues")
         print()
 
-def send_message(host: str('localhost'), queue_name: str ('missybbq'), message: str('producing')):
+def send_message(host: str , queue_name: str , message: str):
     """
     Creates and sends a message to the queue each execution.
     This process runs and finishes.
 
     Parameters:
         host (str): the host name or IP address of the RabbitMQ server
-        queue_name (str): the name of the queue
+        queue_name (str): the name of the queue, one for each channel
         message (str): the message to be sent to the queue
     """
 host = 'localhost'
@@ -67,7 +67,7 @@ for row in reader:
 
 
    # make sure the f is outside of the quotations, made three separate
-   # rows, one for each consume because they all need the time for
+   # rows, one for each consumer because they all need the time for
    # each channel. 
    fstring_message = f"{Time_UTC_}, {Channel1}, {Channel2}, {Channel3}"
 
@@ -85,23 +85,62 @@ try:
     conn = pika.BlockingConnection(pika.ConnectionParameters(host))
     # use the connection to create a communication channel
     ch = conn.channel()
-    # use the channel to declare a durable queue
-    # a durable queue will survive a RabbitMQ server restart
-    # and help ensure messages are processed in order
-    # messages will not be deleted until the consumer acknowledges
-    ch.queue_declare(queue='missybbq', durable=True)
-    # use the channel to publish a message to the queue
-    # every message passes through an exchange
-    ch.basic_publish(exchange="", routing_key='missybbq', body=MESSAGE)
+    #the three queues we will use for the producing.
+    ch.queue_declare(queue=Channel1, durable=True)
+    ch.queue_declare(queue=Channel2, durable=True)
+    ch.queue_declare(queue=Channel3, durable=True)
+        
+try:
+    Channel1 = round(float(Channel1),1)
+    # use an fstring to create a message from our data
+    # notice the f before the opening quote for our string?
+    smoker_temps = f"[{Time_UTC_}, {Channel1}]"
+    # prepare a binary (1s and 0s) message to stream
+    MESSAGE = smoker_temps.encode()
+    # use the socket sendto() method to send the message
+    sock.sendto(MESSAGE, address_tuple)
+    ch.basic_publish(exchange="", routing_key= str, body=MESSAGE)
     # print a message to the console for the user
-    print(f" [x] Sent {MESSAGE}")
+    print(f" [x] Sent Smoker Temp {MESSAGE}")
+except ValueError:
+        pass
+            
+try:
+    Channel2 = round(float(Channel2),1)
+    # use an fstring to create a message from our data
+    # notice the f before the opening quote for our string?
+    Food_A = f"[{Time_UTC_}, {Channel2}]"
+    # prepare a binary (1s and 0s) message to stream
+    MESSAGE2 = Food_A.encode()
+    # use the socket sendto() method to send the message
+    sock.sendto(MESSAGE2, address_tuple)
+    ch.basic_publish(exchange="", routing_key=Channel2, body=MESSAGE2)
+    # print a message to the console for the user
+    print(f" [x] Sent Food A Temp {MESSAGE2}")
+except ValueError:
+        pass
+
+try:
+    Channel3 = round(float(Channel3),1)
+    # use an fstring to create a message from our data
+    # notice the f before the opening quote for our string?
+    Food_B = f"[{Time_UTC_}, {Channel3}]"
+    # prepare a binary (1s and 0s) message to stream
+    MESSAGE3 = Food_B.encode()
+    # use the socket sendto() method to send the message
+    sock.sendto(MESSAGE3, address_tuple)
+    ch.basic_publish(exchange="", routing_key=Channel3, body=MESSAGE3)
+    # print a message to the console for the user
+    print(f" [x] Sent Food B Temp {MESSAGE3}")
+except ValueError:
+        pass
 
 except pika.exceptions.AMQPConnectionError as e:
-    print(f"Error: Connection to RabbitMQ server failed: {e}")
-    sys.exit(1)
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        sys.exit(1)
 finally:
-    # close the connection to the server
-    conn.close()
+        # close the connection to the server
+        conn.close()
 
 # Standard Python idiom to indicate main program entry point
 # This allows us to import this module and use its functions
@@ -117,4 +156,4 @@ if __name__ == "__main__":
     # join by the space character inside the quotes
     message = " ".join(sys.argv[1:]) or "{message}"
     # send the message to the queue
-    send_message(host,'missybbq',message)
+    send_message ("localhost", "01-smoker_temps", "02-Food_A", "03-Food_B", message)
